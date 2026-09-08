cc3cb91218097cf3c5c201b05f9bcb30a21bf932
Goutam3419 <156918546+Goutam3419@users.noreply.github.com>
2026-09-08 18:32:46 +0530

    Add HomeSections component with multiple sections

--- reviewhub-framework-v1/frontend/components/sections/HomeSections.tsx
+++ reviewhub-framework-v1/frontend/components/sections/HomeSections.tsx
@@ -1,1 +1,17 @@
+import Categories from "./Categories";
+import FeaturedBrands from "./FeaturedBrands";
+import TrendingReviews from "./TrendingReviews";
+import Newsletter from "./Newsletter";
+import Footer from "../layout/Footer";
 
+export default function HomeSections() {
+  return (
+    <>
+      <Categories />
+      <FeaturedBrands />
+      <TrendingReviews />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
